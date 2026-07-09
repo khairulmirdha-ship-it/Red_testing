363f735e5c004dc901c2227b97755a50e4ef7a77
khairulmirdha-ship-it <khairul.mirdha@gmail.com>
2026-07-09 12:04:10 +1000

    Add files via upload

---
+++ funny thingy.py
@@ -0,0 +1,66 @@
+# Write your code here =)
+killer = input("Who killed Snowstorm? ")
+
+if killer == "Moon":
+    print("suspecting Moon? That's actually a pretty good idea. He is manipulative.")
+elif killer == "Bliz":
+    print("She doesn't even think Snowstorm is dead.")
+elif killer == "Herself":
+    print("Far too happy, no")
+elif killer == "Mimikate":
+    print("Who's that?")
+elif killer == "Harvey":
+    print("Ding! you got it =)")
+elif killer == "Mureader":
+    print("Have they even met?")
+elif killer == "Carnavilla":
+    print("Moon thought that at one point aswell, you know")
+elif killer == "Red":
+    print("You are talking to them right now, you know")
+elif killer == "Is that a Moon referance?":
+    print("Neverending night slop")
+elif killer == "Weaver":
+    print("Good guess, but nah")
+elif killer == "Me":
+    print("Don't paint yourself in fake blood now...")
+elif killer == "Gaster":
+    print("It's rude to bring up somone who's watching")
+elif killer == "Kai":
+    print("Wrong universe, buddy")
+elif killer == "May":
+    print("Wrong universe, buddy")
+elif killer == "Infected Moon":
+    print("How do you know that exists? And no, not him")
+elif killer == "Sunny":
+    print("Niche guess. No.")
+else:
+    print("This person was not an option")
+
+location = input("Do you know where she could've died? ")
+if location == "Her home":
+    print("Her family would've found her corpse by now")
+elif location == "Carnavilla's stomach":
+    print("That's a very valid guess. But the valid ones aren't always true")
+elif location == "Mimikate's bar":
+    print("What bar? Oh yeah, that bar. Nope")
+elif location == "Shining town":
+    print("Well now we're getting somewhere. Try being more specific, though")
+elif location == "The alley":
+    print("but Moon was already there, it can't be...")
+    print("CHECK AGAIN")
+    print("Oh fine...")
+    print("so?")
+    print("It's there.")
+elif location == "in the place with purple flowers":
+    print("So romantic. But also so wrong")
+elif location == "Weaver's hideout":
+    print == ("Smart at first glance. Not so much on second")
+elif location == "Purple's lab":
+    print == ("oh, it certainly would serve his interests, allow me to check")
+    print == ("...")
+    print == ("Yeah.. no. Nice try though")
+elif location == "The living library":
+    print("The librarian would've prevented that")
+elif location == "I don't know":
+    print("Failiure. You'll never avenge her at this rate.")
+
